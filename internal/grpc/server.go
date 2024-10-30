7f069a7786ade7ea2c6a9bab9ccaab0ec0fea6fe
Serhii Dmytrenko <dmmytrenko@gmail.com>
2024-10-30 22:55:22 +0200

    graceful shutdown implemented

--- internal/grpc/server.go
+++ internal/grpc/server.go
@@ -1,8 +1,13 @@
 package grpc
 
 import (
+	"context"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/dmmitrenko/card-validator/cards"
 	"github.com/dmmitrenko/card-validator/internal/config"
@@ -13,10 +18,14 @@ import (
 
 type gRPCServer struct {
 	address string
+	server  *grpc.Server
 }
 
 func NewGRPCServer(address string) *gRPCServer {
-	return &gRPCServer{address: address}
+	return &gRPCServer{
+		address: address,
+		server:  grpc.NewServer(grpc.UnaryInterceptor(middleware.UnaryInterceptor())),
+	}
 }
 
 func (s *gRPCServer) Run() error {
@@ -24,15 +33,34 @@ func (s *gRPCServer) Run() error {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	grpcServer := grpc.NewServer(
-		grpc.UnaryInterceptor(middleware.UnaryInterceptor()),
-	)
 
 	cfg := config.NewConfig()
 	apiClient := cards.NewApiClient(cfg.APIURL)
 	cardValidator := cards.NewCardValidator(apiClient)
-	handler.NewCardValidatorHandler(grpcServer, cardValidator)
+	handler.NewCardValidatorHandler(s.server, cardValidator)
 
 	log.Println("Starting gRPC server on", s.address)
-	return grpcServer.Serve(listener)
+
+	go func() {
+		if err := s.server.Serve(listener); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	s.waitForShutdown()
+	return nil
+}
+
+func (s *gRPCServer) waitForShutdown() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+	log.Println("Shutting down gRPC server...")
+
+	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	s.server.GracefulStop()
+	log.Println("gRPC server stopped gracefully")
 }
